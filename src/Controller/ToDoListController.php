375a30f33bff660eca2ff1ad3e91f42b6eb0e8ab
Yulia Dubenchuk <yulia.dubenchuk@ab-soft.net>
2023-01-20 18:42:59 +0200

    Lesson 107 - 109 | Installation, add html template and create controller

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_to_do_list")
+     */
+    public function index(): Response
+    {
+        return $this->render('to_do_list/index.html.twig', [
+            'controller_name' => 'ToDoListController',
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create(): Response
+    {
+        exit('to do: create a new task!');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id): Response
+    {
+        exit('to do: switch status of the task!' . $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     */
+    public function delete($id): Response
+    {
+        exit('to do: delete a task with the id of ' . $id);
+    }
+}
